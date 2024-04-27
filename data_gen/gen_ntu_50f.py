75c6e924ca66055b4362073dcea9560a04452a35
ben0i0d <ben0i0d@foxmail.com>
2024-04-27 16:16:33 +0800

    repair somethine

--- data_gen/gen_ntu_50f.py
+++ data_gen/gen_ntu_50f.py
@@ -8,34 +8,16 @@ import numpy as np
 from numpy.lib.format import open_memmap
 from NTUDatasets import NTUMotionProcessor
 
+from tqdm import tqdm
 
 max_body = 2
 num_joint = 25
 max_frame = 50
 batch_size = 64
 
-toolbar_width = 30
-
-
-def print_toolbar(rate, annotation=''):
-    # setup toolbar
-    sys.stdout.write("{}[".format(annotation))
-    for i in range(toolbar_width):
-        if i * 1.0 / toolbar_width > rate:
-            sys.stdout.write(' ')
-        else:
-            sys.stdout.write('-')
-        sys.stdout.flush()
-    sys.stdout.write(']\r')
-
-
-def end_toolbar():
-    sys.stdout.write("\n")
-
-
 def gendata(dataset_path, out_path, benchmark, part='eval'):
     dataset = NTUMotionProcessor(
-        '{}/{}_data.npy'.format(os.path.join(dataset_path, benchmark), part),
+        '{}/{}_joint.npy'.format(os.path.join(dataset_path, benchmark), part),
         '{}/{}_label.pkl'.format(os.path.join(dataset_path, benchmark), part),
         data_type='relative',
         t_length=max_frame,
@@ -52,29 +34,14 @@ def gendata(dataset_path, out_path, benchmark, part='eval'):
         num_workers=1,
         drop_last=False)
 
-    f_position = open_memmap(
-        '{}/{}_position.npy'.format(out_path, part),
-        dtype='float32',
-        mode='w+',
-        shape=(dataset.N, 3, max_frame, num_joint, max_body))
+    f_position = open_memmap('{}/{}_position.npy'.format(out_path, part),dtype='float32',mode='w+',shape=(dataset.N, 3, max_frame, num_joint, max_body))
 
-    f_motion = open_memmap(
-        '{}/{}_motion.npy'.format(out_path, part),
-        dtype='float32',
-        mode='w+',
-        shape=(dataset.N, 3, max_frame, num_joint, max_body))
+    f_motion = open_memmap('{}/{}_motion.npy'.format(out_path, part),dtype='float32',mode='w+',shape=(dataset.N, 3, max_frame, num_joint, max_body))
 
-    f_label = open_memmap(
-        '{}/{}_label.npy'.format(out_path, part),
-        dtype='int64',
-        mode='w+',
-        shape=(dataset.N, 1))
+    f_label = open_memmap('{}/{}_label.npy'.format(out_path, part),dtype='int64',mode='w+',shape=(dataset.N, 1))
 
     index = 0
-    for i, (data, motion, label) in enumerate(data_loader):
-        print_toolbar(i * 1.0 / len(data_loader),
-                      '({:>5}/{:<5}) Processing {:>5}-{:<5} data: '.format(
-                          i + 1, len(data_loader), benchmark, part))
+    for i, (data, motion, label) in tqdm(enumerate(data_loader)):
         length = label.shape[0]
         if i * batch_size != index:
             print(i, index)
@@ -82,13 +49,11 @@ def gendata(dataset_path, out_path, benchmark, part='eval'):
         f_motion[index:(index+length), :, :, :, :] = motion.numpy()
         f_label[index:(index+length), :] = label.numpy().reshape(-1, 1)
         index += length
-    end_toolbar()
-
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')
-    parser.add_argument('--dataset_path', default='data/nturgb+d_skeletons')
+    parser.add_argument('--dataset_path', default='data/')
     parser.add_argument('--out_folder', default='data/f50')
 
     benchmark = ['xsub', 'xview']
