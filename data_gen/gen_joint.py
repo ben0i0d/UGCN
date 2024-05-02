5e112a412590f2a39b0215c1cf24b8fa041f5836
ben0i0d <ben0i0d@foxmail.com>
2024-05-02 22:01:50 +0800

    split 300f to a dir

--- data_gen/gen_joint.py
+++ data_gen/gen_joint.py
@@ -114,7 +114,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')
     parser.add_argument('--data_path', default='data/nturgb+d_skeletons')
     parser.add_argument('--ignored_sample_path',default='NTU_RGBD_samples_with_missing_skeletons.txt')
-    parser.add_argument('--out_folder', default='data')
+    parser.add_argument('--out_folder', default='data/f300')
 
     benchmark = ['xsub', 'xview']
     part = ['train', 'val']
